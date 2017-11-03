cfa996f7e57f4236408fb9e7a57cb24da586208e
Arthur Cordova <arthur.stapassoli@gmail.com>
2017-11-03 17:20:50 -0200

    Table View.

--- PeleMedicalCenter/PeleMedicalCenter/MedicViewController.swift
+++ PeleMedicalCenter/PeleMedicalCenter/MedicViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class MedicViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
