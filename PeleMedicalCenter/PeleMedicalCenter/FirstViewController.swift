9c1a23a4aaec9062dd21dfdef6986216bc9a221c
Arthur Cordova <arthur.stapassoli@gmail.com>
2017-11-03 08:49:54 -0200

    Init project

---
+++ PeleMedicalCenter/PeleMedicalCenter/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PeleMedicalCenter
+//
+//  Created by Arthur on 03/11/17.
+//  Copyright © 2017 Mobway. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
